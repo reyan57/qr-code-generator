20136a0c3d97026b24580953c50eebeb9ca68478
reyan <113667658+reyan57@users.noreply.github.com>
2022-10-11 19:45:48 +0530

    errors fixed | FINAL

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 // made by reyan :D
 
-const wrapper = document.querySelector("wrapper"),
-qrInput = wrapper.querySelector(".form input",
+const wrapper = document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input"),
 generateBtn = wrapper.querySelector(".form button"),
 qrImg = wrapper.querySelector(".qr-code img");
 let preValue;
-
 generateBtn.addEventListener("click", () => {
-	let qrValue = qrInput.value.trim();
-	if(!qrValue || preValue === qrValue) return;
-	preValue = qrValue;
-	generateBtn.innerText = "Generating QR Code :D";
-	qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
-	qr.Img.addEventListener("load", () => {
-		generateBtn.innerText = "Generate QR Code";
-	});
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
 });
-
 qrInput.addEventListener("keyup", () => {
-	if(!qrInput.value.trim()) {
-		wrapper.classList.remove("active");
-		preValue = "";
-	}
+    if(!qrInput.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue = "";
+    }
 });
 
 // idk why i am using notepad++ , don't mind it AHAHAHAHA :')
